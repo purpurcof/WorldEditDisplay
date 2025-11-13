751871b3004c7a7dae8643b6dabd25e2652a5a57
TWME <65117253+TWME-TW@users.noreply.github.com>
2025-11-13 09:40:00 +0800

    chore: 更新 InboundPacketListener.java 檔案

--- src/main/java/dev/twme/worldeditdisplay/listener/InboundPacketListener.java
+++ src/main/java/dev/twme/worldeditdisplay/listener/InboundPacketListener.java
@@ -20,7 +20,15 @@ public class InboundPacketListener implements PacketListener {
         if (event.getPacketType() != PacketType.Play.Client.PLUGIN_MESSAGE) {
             return;
         }
-        WrapperPlayClientPluginMessage packet = new WrapperPlayClientPluginMessage(event);
+
+        // 為了防止像是 Axiom 之類的模組封包大小過大導致的錯誤，使用 try-catch 包起來
+        // https://github.com/retrooper/packetevents/issues/1133
+        WrapperPlayClientPluginMessage packet = null;
+        try {
+            packet = new WrapperPlayClientPluginMessage(event);
+        } catch (Exception e) {
+            return;
+        }
 
         // 監聽 minecraft:register 頻道，檢查是否包含 worldedit:cui
         if (Constants.REGISTER_CHANNEL.equals(packet.getChannelName())) {
@@ -29,7 +37,7 @@ public class InboundPacketListener implements PacketListener {
             
             // 檢查是否包含 worldedit:cui
             if (registerMessage.contains(Constants.CUI_CHANNEL)) {
-                Player player = (Player) event.getPlayer();
+                Player player = event.getPlayer();
                 PlayerData playerData = PlayerData.getPlayerData(player);
                 // 標記玩家已經有 CUI
                 playerData.setCuiEnabled(true);
